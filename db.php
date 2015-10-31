f6749765f415b38b0fafc8e834eb699839f12bbd
Tay Kai Teng Jorel <JorelTay@r-200-102-25-172.comp.nus.edu.sg>
2015-10-31 20:00:06 +0800

    fuck up

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+//File Description:  PHP Process for Database connection string
+
+session_start();
+	 $username = "root";
+     $password = "";
+     $hostname = "localhost";
+	 
+	//connect to database ["hostingweb","hostname","password","databasename"
+	$mysqli=mysqli_connect($hostname,$username,$password,"crowdfunding");
+	
+	// Check connection
+	if (mysqli_connect_errno())
+  	{
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ 	}
+	
+?>
