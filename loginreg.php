a33eaacc43cb2201e991ba3a7c2e28fd8d19d190
Woan Chin <woanchin.tee@gmail.com>
2015-11-11 19:35:12 +0800

    changes made to footer and weird weeird merge comments

--- loginreg.php
+++ loginreg.php
@@ -391,50 +391,6 @@
     </div>
 
 
-    <div class="footer">
-        <div class="container">
-            <div class="col-sm-2">
-                <h5>Site Map</h5>
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Services</a></li>
-                    <li><a href="">Pricing</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </div>
-            <div class="col-sm-4 col-md-3 twitter">
-                <h5>Twitter Feed</h5>
-                <ul>
-                    <li><i class="fa  fa-twitter"></i>Lorem ipsum dolor sit amet,  adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna erat volutpat. <span>http://uibrush.com</span></li>
-                    <li><i class="fa  fa-twitter"></i>Lorem ipsum dolor sit amet,  adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna erat volutpat. <span>http://uibrush.com</span></li>
-                </ul>
-            </div>
-            <div class="col-md-4 testimonial">
-                <h5>Testimonial</h5>
-                <ul>
-                    <li><i class="fa  fa-quote-left"></i>Lorem ipsum dolor sit amet,  adipiscing elit, sed 
-  							diam  nibh euismod tincidunt ut laoreet dolore magna erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  </li>
-
-                </ul>
-                <div class="t-image">
-                    <img src="img/testimonial.png">
-                    <h6>- Michael R. Hanke</h6>
-                    <br>
-                    <p>ceo of sun</p>
-                </div>
-            </div>
-            <div class="col-md-3 contact-footer">
-                <div class="footer-form">
-                    <h5>Contact Us</h5>
-                    <input type="text" placeholder="Name">
-                    <input type="text" placeholder="E-Mail">
-                    <textarea placeholder="Message" rows="3"></textarea>
-                    <button class="submit-bt">Send</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="copyright">
         <div class="container">
             <p>All Rights Reserved 2015 &copy; Espoir.com</p>
